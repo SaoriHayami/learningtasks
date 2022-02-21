d962977a958d6a57435604485ac3366c048f4090
Anton <100029027+SaoriHayami@users.noreply.github.com>
2022-02-21 08:42:15 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+ <head>
+ <title> My Homepage </title>
+ </head>
+ <body>
+<h1> Welcome to My Website! I am Joshua Anton Magbanua </h1>
+<ul>
+<li><a href="index.php">home</a></li>
+<li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+</ul>
+</body>
+</tml>
