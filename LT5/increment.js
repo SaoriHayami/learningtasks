05165f0b0dbf8e21a55d9eb7111b2fe25b5f4f3b
Anton <100029027+SaoriHayami@users.noreply.github.com>
2022-04-14 22:58:03 +0800

    Add files via upload

---
+++ LT5/increment.js
@@ -0,0 +1,12 @@
+function incrementButton() {
+    var element = document.getElementById('incrementText');
+    var value = element.innerHTML;
+    
+    ++value;
+    
+    console.log(value);
+    document.getElementById('incrementText').innerHTML = value;
+}
+  function myFunction() {
+    alert("(ಥ﹏ಥ) Thank you for the food");
+    }
